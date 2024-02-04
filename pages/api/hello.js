00e309d114ea6f599cdf352837da3bc313aa9cd9
Bin <xuebinb@gmail.com>
2024-02-04 11:42:27 -0500

    for deploy

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 export default async function handler(req, res) {
   const name = req.body.pet
